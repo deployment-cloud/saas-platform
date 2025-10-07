a387d51471f3e1d85be995a66fc0805594c0aca9
deployment-cloud <najeebkhan87@gmail.com>
2025-10-08 03:53:33 +0500

    comit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: false // Removed warning
-  }
+  swcMinify: true,
+  // Any other Next.js settings can go here
 };
 
 export default nextConfig;
